55f3870a43d3912c661016cf6339e7bd9f93370d
yoanastamenova <yoanabarbie@icloud.com>
2024-06-10 12:40:57 +0200

    kickoff node project

---
+++ friend.js
@@ -0,0 +1,23 @@
+
+function Sumar(a, b) {
+    return a + b;
+}
+
+function Restar (a, b) {
+   return a - b;
+}
+
+function Multiplicar (a, b) {
+    return a * b;
+}
+
+function Dividir (a,b) {
+    return a/b;
+}
+
+module.exports = {
+    Sumar,
+    Restar,
+    Multiplicar,
+    Dividir
+}
---
+++ index.js
@@ -0,0 +1,5 @@
+let {Multiplicar, Dividir} = require("./friend");
+
+let resultado = Multiplicar(1,4)
+
+console.log(resultado)
